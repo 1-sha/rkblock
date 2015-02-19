241fe69ae26f84ea1303021496d6fe8154bc1fd5
Peshmelba <contact.peshmelba@gmail.com>
2015-02-19 20:07:01 +0100

    Delete messages on profils

--- content-script/rkblock.js
+++ content-script/rkblock.js
@@ -22,7 +22,8 @@ window.onload = main;
 function main()
 {
 	console.log("RKBLOCK is running on this page.");
-
+	
+// Delete on fanfic comments
 	comments = document.getElementsByClassName('commentBloc');
 	if (comments.length > 0)
 	{
@@ -46,10 +47,32 @@ function main()
 				}
 			}
 		}
+	}	
+
+// Delete on profil comments
+	comments = document.getElementsByClassName('profilComment');
+	if (comments.length > 0)
+	{
+		for (i = 0 ; i < comments.length ; i++)
+		{
+			comment = comments[i];
+			pseudo = comment.getElementsByTagName('a');
+
+			if (pseudo.length > 0)
+			{
+				pseudo = pseudo[0];
+				if (pseudo.href.match(user) != null)
+				{
+					rkMessages.push(comment);
+				}
+			}
+		}
 	}
 
 	for (i = 0 ; i < rkMessages.length ; i++)
 	{
 		rkMessages[i].parentNode.removeChild(rkMessages[i]);
 	}
+
+	rkMessages = [];
 }
